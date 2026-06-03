aa580309193a3c15f17fde86ac14828a9b1b9f6f
MarinaHurricane <maryishch88@gmail.com>
2026-06-04 00:08:18 +0100

    json script change

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -4,6 +4,7 @@ export const connectMongoDB = async () => {
   try {
     const mongoUrl = process.env.MONGO_URL;
     await mongoose.connect(mongoUrl);
+    console.log('Mongo URL:', process.env.MONGO_URL);
     console.log('✅ Database connection success');
   } catch (error) {
     console.log('Error connecting to Database', error.massage);
