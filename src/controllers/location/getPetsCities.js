11662af7e461a180da77d8b21a7f7dd1576def66
MarinaHurricane <maryishch88@gmail.com>
2026-06-15 00:39:40 +0300

    handling location search with ""

--- src/controllers/location/getPetsCities.js
+++ src/controllers/location/getPetsCities.js
@@ -1,7 +1,11 @@
 import { City } from '../../models/city.js';
 
 export const getPetsCities = async (req, res) => {
-  const { search } = req.query;
+  const { search= "" } = req.query;
+
+  if(!search.trim()) {
+    return res.status(200).json([]);
+  }
 
   const locations = await City.find({
     city: {
