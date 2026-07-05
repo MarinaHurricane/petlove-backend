652779fbb0ac6083f6cf6d9769096ba863bd46ff
MarinaHurricane <maryishch88@gmail.com>
2026-07-05 16:05:45 +0100

    added cors originand credentials

--- src/server.js
+++ src/server.js
@@ -20,7 +20,10 @@ const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173/",
+  credentials: true,
+}));
 app.use(
   pino({
     level: 'info',
