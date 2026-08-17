557668cb6c65c78fba0d838e0259124a7027e06b
MarinaHurricane <maryishch88@gmail.com>
2026-08-17 19:15:35 +0100

    change pet schema

--- src/models/pet.js
+++ src/models/pet.js
@@ -4,62 +4,66 @@ import { CATEGORIES } from '../constants/categories.js';
 import { GENDER } from '../constants/gender.js';
 import { City } from './city.js';
 
-const petSchema = new Schema({
-  _id: {
-    type: Schema.Types.ObjectId,
+const petSchema = new Schema(
+  {
+    _id: {
+      type: Schema.Types.ObjectId,
+    },
+    species: {
+      type: String,
+      enum: SPECIES,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: CATEGORIES,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    birthday: {
+      type: Date,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: GENDER,
+      required: true,
+    },
+    location: {
+      type: String,
+      ref: City,
+      required: true,
+    },
+    imgURL: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    popularity: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+    },
   },
-  species: {
-    type: String,
-    enum: SPECIES,
-    required: true,
+  {
+    timestamps: true,
   },
-  category: {
-    type: String,
-    enum: CATEGORIES,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  birthday: {
-    type: Date,
-    required: true,
-  },
-  comment: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    enum: GENDER,
-    required: true,
-  },
-  location: {
-    type: String,
-    ref: City,
-    required: true,
-  },
-  imgURL: {
-    type: String,
-    required: true,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  popularity: {
-    type: Number,
-    required: true,
-  },
-
-}, {
-  timestamps: true,
-},
 );
 
 export const Pet = model('Pet', petSchema);
