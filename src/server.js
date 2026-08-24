d549ad1543b6c0ea76ba1fab74f5ed2dea1087b9
MarinaHurricane <maryishch88@gmail.com>
2026-08-24 17:23:06 +0100

    cors fix

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      'https://https://petlove-frontend.vercel.app',
+      'https://petlove-frontend.vercel.app',
     ],
     credentials: true,
   }),
